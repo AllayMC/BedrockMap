b67fe5961fe2f8b92f96f6df0d6862703d5cb37f
epicbigcat <184706652+epicbigcat@users.noreply.github.com>
2025-03-15 22:47:37 +0800

    cleanup.

--- scripts/build.ps1
+++
@@ -1,4 +0,0 @@
-$build_dir = "./build"
-# 编译
-cmake -G "MinGW Makefiles" -B  $build_dir .
-cmake --build $build_dir --config Debug -j 18 -- 
--- scripts/build_run.ps1
+++
@@ -1,4 +0,0 @@
-$build_dir = "./build"
-cmake --build $build_dir --config Debug -j 18 --
-# 运行
-Start-Process -FilePath  "./$build_dir/BedrockMap.exe"  -WorkingDirectory $build_dir
